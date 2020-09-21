9469e9cd584c8703b4798a852a601f092222af9f
Stian Johansen <stian.a.johansen@gmail.com>
2020-09-21 15:41:06 +0200

    bah

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -10,16 +12,18 @@ class GamesController < ApplicationController
     @letters = params[:letters]
     @valid = in_grid?(@answer, @letters)
     @word_found = in_api?(@answer) if @valid
+    @calculated_score = (@answer.size * 12) - @calculated_time
   end
 
   def in_grid?(attempt, grid)
     attempt.upcase.chars.all? { |l| attempt.upcase.count(l) <= grid.count(l) }
   end
 
-  def in_api?(word)
+  def in_api?(attempt)
     url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
-    read_url = open(url).read
+    read_url = RestClient.get(url)
     word = JSON.parse(read_url)
 
     word['found']
+  end
 end
